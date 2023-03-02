2162888e2c45a251714ccd3abc1bc22939f0b46a
Nataliia-git <119956364+Nataliia-git@users.noreply.github.com>
2023-03-02 19:36:08 +0100

    Integrated Bootstrap, styled the SearchEngine

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -30,8 +30,24 @@ export default function SearchEngine() {
 
   let form = (
     <form onSubmit={handleSubmit}>
-      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
-      <button type="Submit">Search</button>
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            className="form-control"
+            autoComplete="off"
+            onChange={updateCity}
+          />
+        </div>
+        <div className="col-3">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary w-100"
+          />
+        </div>
+      </div>
     </form>
   );
 
